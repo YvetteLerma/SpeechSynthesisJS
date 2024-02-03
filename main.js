bb5256406aaff6da1be9c8d53e41bdfef3ef12d2
Yvette Lerma <lerma.yvette@hotmail.com>
2024-02-03 13:09:25 -0700

    working on speech and pitch

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+     voices = this.getVoices();
+     voicesDropdown.innerHTML = voices
+        .map(voices => `<option value="${voices.name}">${voices.name} (${voices.lang})</option>`)
+        .join('');
+};
+
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+}
+
+function toggle() {
+    speechSynthesis.cancel();
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
